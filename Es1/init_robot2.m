4c43759af9f7ce612a803cc60dc0ef9150985d4a
Elisa Martinenghi <s6504193@studenti.unige.it>
2026-05-31 16:27:33 +0200

    2R robot ex done

---
+++ Es1/init_robot2.m
@@ -0,0 +1,22 @@
+% Parameters
+g = [0, 0, -9.81]; % gravity
+
+% Structure
+lenght_l1 = 0.5; % m
+lenght_l2 = 0.4; % m
+
+mass_l1 = 1; % kg
+mass_l2 = 1; % kg
+
+width_links = 0.01; % m
+
+lenght_cyl = 0.07; % m
+radius_cyl = 0.015; % m
+
+radius_sphere = 0.015; % m
+
+corner_base = 0.15; % m
+
+% Initial position
+q1_init = 0; % deg
+q2_init = 0; % deg
